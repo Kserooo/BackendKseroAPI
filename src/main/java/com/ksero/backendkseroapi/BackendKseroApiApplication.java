3bdbe7629cce46a7eaff435890afee3ae50765e2
Draacckeer <u201922975@upc.edu.pe>
2022-05-31 15:26:38 -0500

    Add initial files

---
+++ src/main/java/com/ksero/backendkseroapi/BackendKseroApiApplication.java
@@ -0,0 +1,13 @@
+package com.ksero.backendkseroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendKseroApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendKseroApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ksero/backendkseroapi/BackendKseroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksero.backendkseroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendKseroApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
