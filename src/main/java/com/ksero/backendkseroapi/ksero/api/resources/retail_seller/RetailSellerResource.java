af9aee13b73c51ed473d0d793513a215d5a45fb0
David Reategui <reateguicevallos.darh@gmail.com>
2022-05-31 18:13:01 -0500

    Added Retail Seller and Wholesaler Create and Update Resources

--- src/main/java/com/ksero/backendkseroapi/ksero/api/resources/retail_seller/RetailSellerResource.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/api/resources/retail_seller/RetailSellerResource.java
@@ -1,4 +1,23 @@
 package com.ksero.backendkseroapi.ksero.api.resources.retail_seller;
 
+import lombok.*;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
 public class RetailSellerResource {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Date birthday;
+    private int phone;
+    private String email;
+    private String address;
+    private String username;
+    private String password;
+    private String description;
 }
