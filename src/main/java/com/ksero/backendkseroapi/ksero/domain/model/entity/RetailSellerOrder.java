416349c33c1a0f83a67d92c5956fd51fdff9eb06
Draacckeer <u201922975@upc.edu.pe>
2022-05-31 17:23:55 -0500

    Add Model Entity
    Added RetailSellerOrder and WholesalerOrder

--- src/main/java/com/ksero/backendkseroapi/ksero/domain/model/entity/RetailSellerOrder.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/domain/model/entity/RetailSellerOrder.java
@@ -1,4 +1,38 @@
 package com.ksero.backendkseroapi.ksero.domain.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@With
+@Entity
+@Table(name = "retail_seller_order")
 public class RetailSellerOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Positive
+    private Long quantity;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "retail_seller_id",nullable = false)
+    @JsonIgnore
+    private RetailSeller retailSeller;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    @JsonIgnore
+    private Product product;
 }
