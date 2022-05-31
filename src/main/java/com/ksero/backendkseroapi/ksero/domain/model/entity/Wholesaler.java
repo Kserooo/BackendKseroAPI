2a8a8717a74a153cb5be516826f473f1ed737f5b
David Reategui <reateguicevallos.darh@gmail.com>
2022-05-31 17:07:11 -0500

    Added Retail Seller and Wholesaler Models

--- src/main/java/com/ksero/backendkseroapi/ksero/domain/model/entity/Wholesaler.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/domain/model/entity/Wholesaler.java
@@ -1,4 +1,51 @@
 package com.ksero.backendkseroapi.ksero.domain.model.entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
 public class Wholesaler {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private String firstName;
+
+    @NotNull
+    @NotBlank
+    private String lastName;
+
+    @NotNull
+    @NotBlank
+    private Date birthday;
+
+    @NotNull
+    @NotBlank
+    private int phone;
+
+    @NotNull
+    @NotBlank
+    private String email;
+
+    @NotNull
+    @NotBlank
+    private String address;
+
+    @NotNull
+    @NotBlank
+    private String username;
+
+    @NotNull
+    @NotBlank
+    private String password;
+
+    @NotNull
+    @NotBlank
+    private String description;
 }
