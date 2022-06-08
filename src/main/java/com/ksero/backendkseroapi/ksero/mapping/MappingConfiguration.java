a9648cace39ae51e272718d1d92b7750e23bcea0
Draacckeer <u201922975@upc.edu.pe>
2022-06-08 17:39:28 -0500

    Add: Mapping Configuration

--- src/main/java/com/ksero/backendkseroapi/ksero/mapping/MappingConfiguration.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/mapping/MappingConfiguration.java
@@ -1,4 +1,29 @@
 package com.ksero.backendkseroapi.ksero.mapping;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("kseroMappingConfiguration")
 public class MappingConfiguration {
+
+    @Bean
+    public ProductMapper productMapper() {
+        return new ProductMapper();
+    }
+
+    @Bean
+    public RetailSellerMapper retailSellerMapper() { return new RetailSellerMapper(); }
+
+    @Bean
+    public RetailSellerOrderMapper retailSellerOrderMapper() {
+        return new RetailSellerOrderMapper();
+    }
+    @Bean
+    public WholesalerMapper wholesalerMapper() {
+        return new WholesalerMapper();
+    }
+    @Bean
+    public WholesalerOrderMapper wholesalerOrderMapper() { return new WholesalerOrderMapper(); }
+
+
 }
