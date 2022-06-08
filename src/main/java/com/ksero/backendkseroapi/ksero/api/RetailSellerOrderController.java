619ea12ec6680512486c8b489936c33500a518bf
Draacckeer <u201922975@upc.edu.pe>
2022-06-08 16:45:19 -0500

    Add: User Order Controllers

--- src/main/java/com/ksero/backendkseroapi/ksero/api/RetailSellerOrderController.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/api/RetailSellerOrderController.java
@@ -1,4 +1,41 @@
 package com.ksero.backendkseroapi.ksero.api;
 
+import com.ksero.backendkseroapi.ksero.domain.service.RetailSellerOrderService;
+import com.ksero.backendkseroapi.ksero.mapping.RetailSellerOrderMapper;
+import com.ksero.backendkseroapi.ksero.resources.retail_seller_order.CreateRetailSellerOrderResource;
+import com.ksero.backendkseroapi.ksero.resources.retail_seller_order.RetailSellerOrderResource;
+import com.ksero.backendkseroapi.ksero.resources.retail_seller_order.UpdateRetailSellerOrderResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 public class RetailSellerOrderController {
+
+    private final RetailSellerOrderService retailSellerOrderService;
+    private final RetailSellerOrderMapper mapper;
+
+    public RetailSellerOrderController(RetailSellerOrderService retailSellerOrderService, RetailSellerOrderMapper mapper){
+        this.retailSellerOrderService = retailSellerOrderService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping("{retailSellerOrderId}")
+    public RetailSellerOrderResource getRetailSellerOrderById(@PathVariable Long retailSellerOrderId){
+        return mapper.toResource(retailSellerOrderService.getById(retailSellerOrderId));
+    }
+
+    @PostMapping
+    public RetailSellerOrderResource createRetailSellerOrder(@RequestBody CreateRetailSellerOrderResource resource){
+        return mapper.toResource(retailSellerOrderService.create(mapper.toModel(resource)));
+    }
+
+    @PutMapping("{retailSellerOrderId}")
+    public RetailSellerOrderResource updateWholesalerOrder(@PathVariable Long retailSellerOrderId,
+                                                         @RequestBody UpdateRetailSellerOrderResource resource){
+        return mapper.toResource(retailSellerOrderService.update(retailSellerOrderId, mapper.toModel(resource)));
+    }
+
+    @DeleteMapping("{retailSellerOrderId}")
+    public ResponseEntity<?> deleteRetailSeller(@PathVariable Long retailSellerOrderId){
+        return retailSellerOrderService.delete(retailSellerOrderId);
+    }
 }
--- src/main/java/com/ksero/backendkseroapi/ksero/api/WholesalerOrderController.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/api/WholesalerOrderController.java
@@ -1,4 +1,42 @@
 package com.ksero.backendkseroapi.ksero.api;
 
+import com.ksero.backendkseroapi.ksero.domain.service.WholesalerOrderService;
+import com.ksero.backendkseroapi.ksero.mapping.WholesalerOrderMapper;
+import com.ksero.backendkseroapi.ksero.resources.wholesaler_order.CreateWholesalerOrderResource;
+import com.ksero.backendkseroapi.ksero.resources.wholesaler_order.UpdateWholesalerOrderResource;
+import com.ksero.backendkseroapi.ksero.resources.wholesaler_order.WholesalerOrderResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 public class WholesalerOrderController {
+
+    private final WholesalerOrderService wholesalerOrderService;
+    private final WholesalerOrderMapper mapper;
+
+    public WholesalerOrderController(WholesalerOrderService wholesalerOrderService, WholesalerOrderMapper mapper){
+        this.wholesalerOrderService = wholesalerOrderService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping("{wholesalerOrderId}")
+    public WholesalerOrderResource getWholesalerOrderById(@PathVariable Long wholesalerOrderId){
+        return mapper.toResource(wholesalerOrderService.getById(wholesalerOrderId));
+    }
+
+    @PostMapping
+    public WholesalerOrderResource createWholesalerOrder(@RequestBody CreateWholesalerOrderResource resource){
+        return mapper.toResource(wholesalerOrderService.create(mapper.toModel(resource)));
+    }
+
+    @PutMapping("{wholesalerOrderId}")
+    public WholesalerOrderResource updateWholesalerOrder(@PathVariable Long wholesalerOrderId,
+                                         @RequestBody UpdateWholesalerOrderResource resource){
+        return mapper.toResource(wholesalerOrderService.update(wholesalerOrderId, mapper.toModel(resource)));
+    }
+
+    @DeleteMapping("{wholesalerOrderId}")
+    public ResponseEntity<?> deleteWholesaler(@PathVariable Long wholesalerOrderId){
+        return wholesalerOrderService.delete(wholesalerOrderId);
+    }
+
 }
