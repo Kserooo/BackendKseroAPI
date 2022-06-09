e1dc46c7b4c3e9c9eb1bb9c500446c91ab460a1e
Draacckeer <u201922975@upc.edu.pe>
2022-06-09 16:39:56 -0500

    Fixed: Repositories

--- src/main/java/com/ksero/backendkseroapi/ksero/domain/persistence/WholesalerRepository.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/domain/persistence/WholesalerRepository.java
@@ -3,6 +3,8 @@ package com.ksero.backendkseroapi.ksero.domain.persistence;
 import com.ksero.backendkseroapi.ksero.domain.model.entity.Product;
 import com.ksero.backendkseroapi.ksero.domain.model.entity.Wholesaler;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WholesalerRepository extends JpaRepository<Wholesaler,Long> {
 }
