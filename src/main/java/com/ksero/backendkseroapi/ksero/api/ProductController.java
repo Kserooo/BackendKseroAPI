a82ea2afce4edfe0a67d526b502f970426f1e1e2
Draacckeer <u201922975@upc.edu.pe>
2022-06-18 17:36:59 -0500

    Fixed: Mapping

--- src/main/java/com/ksero/backendkseroapi/ksero/api/ProductController.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/api/ProductController.java
@@ -29,7 +29,7 @@ public class ProductController {
     }
 
     @GetMapping
-    public List<ProductResource> getAllProducts(){
+    public List<ProductResource> getAll(){
         return mapper.toResource(productService.getAll());
     }
     @GetMapping("{productId}")
