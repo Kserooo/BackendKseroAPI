d26475559a3366b0647e5058f06a06007fd9543a
Silene Nazareth Casma Rubio <u20201a213@upc.edu.pe>
2022-06-23 10:08:35 -0500

    Add: Mapping Configuration

--- src/main/java/com/ksero/backendkseroapi/security/mapping/MappingConfiguration.java
+++ src/main/java/com/ksero/backendkseroapi/security/mapping/MappingConfiguration.java
@@ -1,5 +1,17 @@
 package com.ksero.backendkseroapi.security.mapping;
 
-public class MappingConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration("securityMappingConfiguration")
+public class MappingConfiguration {
+    @Bean
+    public UserMapper userMapper(){
+        return  new UserMapper();
+    }
+
+    @Bean
+    public RoleMapper roleMapper(){
+        return new RoleMapper();
+    }
 }
