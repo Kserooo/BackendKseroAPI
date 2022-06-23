d8488ccdcc0035759ea4b6100fab1ef0a6ae9166
Draacckeer <u201922975@upc.edu.pe>
2022-06-23 10:44:21 -0500

    Add: Properties

--- src/main/java/com/ksero/backendkseroapi/BackendKseroApiApplication.java
+++ src/main/java/com/ksero/backendkseroapi/BackendKseroApiApplication.java
@@ -1,9 +1,18 @@
 package com.ksero.backendkseroapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@SecurityScheme(name="acme", scheme = "bearer",
+        type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+@OpenAPIDefinition
 @SpringBootApplication
+@EnableJpaAuditing
 public class BackendKseroApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendKseroApiApplication.class, args);
