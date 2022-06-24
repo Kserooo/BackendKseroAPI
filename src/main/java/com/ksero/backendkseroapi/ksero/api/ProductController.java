f43d65ce86f800b07dc7bcd0067aefa9452d3db3
Draacckeer <u201922975@upc.edu.pe>
2022-06-24 13:50:18 -0500

    Fix: ProductController

--- src/main/java/com/ksero/backendkseroapi/ksero/api/ProductController.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/api/ProductController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @SecurityRequirement(name = "acme")
+@CrossOrigin(origins = "*" , maxAge = 3600)
 @RestController
 @RequestMapping("api/v1/products")
 public class ProductController {
