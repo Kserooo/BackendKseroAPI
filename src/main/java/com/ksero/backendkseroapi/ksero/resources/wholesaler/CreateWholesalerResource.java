8bf9f95c8169eb40a59fedbd32f87388c6dea3ba
Draacckeer <u201922975@upc.edu.pe>
2022-06-25 17:24:40 -0500

    Try

--- src/main/java/com/ksero/backendkseroapi/ksero/resources/wholesaler/CreateWholesalerResource.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/resources/wholesaler/CreateWholesalerResource.java
@@ -2,6 +2,8 @@ package com.ksero.backendkseroapi.ksero.resources.wholesaler;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Getter
@@ -10,14 +12,25 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateWholesalerResource {
-
+    @NotNull
     private String firstName;
+    @NotNull
     private String address;
+    @NotNull
     private String lastName;
+    @NotNull
     private String birthday;
+    @NotNull
     private String phone;
+    @NotNull
+    @NotBlank
     private String email;
+    @NotNull
+    @NotBlank
     private String username;
+    @NotNull
+    @NotBlank
     private String password;
+    @NotNull
     private String description;
 }
