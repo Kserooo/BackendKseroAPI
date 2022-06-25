51df4b32b67a6e22000cb82ae74d2e22b475ae7c
Draacckeer <u201922975@upc.edu.pe>
2022-06-25 17:21:47 -0500

    Try

--- src/main/java/com/ksero/backendkseroapi/ksero/api/WholesalerController.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/api/WholesalerController.java
@@ -8,6 +8,8 @@ import com.ksero.backendkseroapi.ksero.resources.wholesaler.WholesalerResource;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/main/java/com/ksero/backendkseroapi/ksero/resources/wholesaler/WholesalerResource.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/resources/wholesaler/WholesalerResource.java
@@ -12,11 +12,11 @@ import java.util.Date;
 public class WholesalerResource {
     private Long id;
     private String firstName;
+    private String address;
     private String lastName;
     private String birthday;
     private String phone;
     private String email;
-    private String address;
     private String username;
     private String password;
     private String description;
