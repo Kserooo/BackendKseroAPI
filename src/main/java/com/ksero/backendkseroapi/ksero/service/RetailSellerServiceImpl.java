764483d448c41791c6c8875022868cb2c511bc5a
Draacckeer <u201922975@upc.edu.pe>
2022-06-25 21:31:56 -0500

    Try

--- src/main/java/com/ksero/backendkseroapi/ksero/service/RetailSellerServiceImpl.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/service/RetailSellerServiceImpl.java
@@ -42,13 +42,8 @@ public class RetailSellerServiceImpl implements RetailSellerService {
 
         if (!violations.isEmpty())
             throw new ResourceValidationException(ENTITY, violations);
-        try{
-            return retailSellerRepository.save(request);
-        }
-        catch (Exception e){
-            throw new ResourceValidationException(ENTITY, "An error occurred while saving client");
-        }
 
+        return retailSellerRepository.save(retailSeller);
     }
 
     @Override
