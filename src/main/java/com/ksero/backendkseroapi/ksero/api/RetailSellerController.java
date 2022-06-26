99bcb259db34c59d0a7d442460c4ed7c4d2d1c6e
Draacckeer <u201922975@upc.edu.pe>
2022-06-26 00:50:59 -0500

    Fix: RetailSellerController Permissions

--- src/main/java/com/ksero/backendkseroapi/ksero/api/RetailSellerController.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/api/RetailSellerController.java
@@ -32,7 +32,7 @@ public class RetailSellerController {
     }
 
     @GetMapping("{retailSellerId}")
-    @PreAuthorize("hasRole('ADMIN') or hasRole('RETAIL_SELLER')")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('RETAIL_SELLER') or hasRole('WHOLESALER')")
     public RetailSellerResource getRetailSellerById(@PathVariable Long retailSellerId){
         return mapper.toResource(retailSellerService.getById(retailSellerId));
     }
