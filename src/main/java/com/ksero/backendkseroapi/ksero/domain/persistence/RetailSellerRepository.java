445ca18e117f9d16e25fff3b6df7538566d13c59
Draacckeer <u201922975@upc.edu.pe>
2022-06-25 22:27:31 -0500

    Try

--- src/main/java/com/ksero/backendkseroapi/ksero/domain/persistence/RetailSellerRepository.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/domain/persistence/RetailSellerRepository.java
@@ -1,6 +1,5 @@
 package com.ksero.backendkseroapi.ksero.domain.persistence;
 
-import com.ksero.backendkseroapi.ksero.domain.model.entity.Product;
 import com.ksero.backendkseroapi.ksero.domain.model.entity.RetailSeller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/ksero/backendkseroapi/ksero/domain/persistence/WholesalerRepository.java
+++ src/main/java/com/ksero/backendkseroapi/ksero/domain/persistence/WholesalerRepository.java
@@ -1,6 +1,5 @@
 package com.ksero.backendkseroapi.ksero.domain.persistence;
 
-import com.ksero.backendkseroapi.ksero.domain.model.entity.Product;
 import com.ksero.backendkseroapi.ksero.domain.model.entity.Wholesaler;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
